7b8c781654b29fdc04d51c2d1ceeff188804e54b
Yared Nebiat <nebiat@usc.edu>
2024-02-09 09:39:16 +0000

    adding test files

---
+++ hw2_tests/testing_utils/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils
+# Build directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("kwsys")
+subdirs("libperf")
---
+++ hw2_tests/testing_utils/kwsys/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils/kwsys
+# Build directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils/kwsys
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
